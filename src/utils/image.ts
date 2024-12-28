c44ab0bf6b368341c44937ed7b14509a89594136
janeipr <jane.indah18063@student.unsika.ac.id>
2024-12-28 19:06:57 +0700

    feat: update image fetching code

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,10 +1,9 @@
 import { getPlaiceholder } from "plaiceholder";
 
 export const getImage = async (src: string, isRemote: boolean = false) => {
-  let buffer: Buffer<ArrayBufferLike>;
   const finalPath = (!isRemote ? process.env.NEXT_PUBLIC_BASEURL : "") + src;
 
-  buffer = await fetch(finalPath).then(async (res) =>
+  const buffer = await fetch(finalPath).then(async (res) =>
     Buffer.from(await res.arrayBuffer())
   );
 
