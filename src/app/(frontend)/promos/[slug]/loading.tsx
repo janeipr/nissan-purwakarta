8bb157330befeec75df58bc90416bde63f66fc09
janeipr <jane.indah18063@student.unsika.ac.id>
2025-04-14 18:58:53 +0700

    feat: loading state

--- src/app/(frontend)/promos/[slug]/loading.tsx
+++ src/app/(frontend)/promos/[slug]/loading.tsx
@@ -2,7 +2,7 @@ import Loading from "@/app/components/Loading";
 
 export default function ServiceLoading() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen flex items-center justify-center">
       <Loading/>
     </div>
   )
